81091945693be8c0746134fc1ec85db3857babac
LydeVN <jules.vannelli12@gmail.com>
2025-09-09 19:39:13 +0200

    add 🎪

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ client.on("interactionCreate", async (interaction) => {
         }
 
         const existingChannel = interaction.guild.channels.cache.find(
-            (c) => c.name === `grp-${groupName.toLowerCase()}`
+            (c) => c.name === `🎪grp-${groupName.toLowerCase()}`
         );
         if (existingChannel) {
             return interaction.editReply("❌ Ce groupe existe déjà !");
